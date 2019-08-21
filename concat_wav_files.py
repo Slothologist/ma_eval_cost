f8bed54d8f10d3babb6ace7d6d3c24a7e6d46f87
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-08-21 16:15:16 +0200

    minimal adjustments to concat script

--- concat_wav_files.py
+++ concat_wav_files.py
@@ -9,12 +9,11 @@ target = 'all_in_one.wav'
 files = [path + x for x in os.listdir(path) if x is not target]
 
 frames_per_second = 16000
-
 timeout_in_seconds = 2. * 400./1000.
+five_twelve_blocks = numpy.ceil(frames_per_second * timeout_in_seconds/512 )
+filler_array = numpy.zeros((five_twelve_blocks*512,), numpy.int16)
 
-filler_array = numpy.zeros((int(frames_per_second * timeout_in_seconds),), numpy.int16)
-
-with sf.SoundFile(path + target, 'w', samplerate=16000, channels=1) as target_file:
+with sf.SoundFile('/home/rfeldhans/Music/' + target, 'w', samplerate=16000, channels=1) as target_file:
     for file in files:
         [target_file.write(block) for block in sf.blocks(file, blocksize=512)]
         target_file.write(filler_array)
